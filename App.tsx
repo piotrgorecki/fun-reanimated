9e284b449d4de70f7d0a7b46a767aa37a61fb702
Piotr Górecki <piotr.gorecki.dev@outlook.com>
2020-09-13 20:32:39 +0200

    First look

--- App.tsx
+++ App.tsx
@@ -3,24 +3,90 @@ import {
   SafeAreaView,
   StyleSheet,
   ScrollView,
-  View,
-  Text,
   StatusBar,
+  TouchableOpacity,
+  View,
 } from "react-native";
+import Animated, {
+  Easing,
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  repeat,
+} from "react-native-reanimated";
 
 const App = () => {
+  const offset = useSharedValue([0, 0]);
+  const viewLayout = useSharedValue([0, 0]);
+  const duration = 600;
+
+  const animatedStyles = useAnimatedStyle(() => {
+    const easing = Easing.inOut(Easing.sin);
+
+    return {
+      transform: [
+        {
+          translateX: withTiming((offset.value[0] ?? 1) * viewLayout.value[0], {
+            duration,
+            easing,
+          }),
+        },
+        {
+          translateY: withTiming((offset.value[1] ?? 1) * viewLayout.value[1], {
+            duration,
+            easing,
+          }),
+        },
+      ],
+    };
+  });
+
   return (
-    <>
+    <View style={styles.container}>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView contentInsetAdjustmentBehavior="automatic">
-          <Text>Hello</Text>
-        </ScrollView>
+      <SafeAreaView style={styles.safeArea}>
+        <View
+          style={styles.content}
+          onLayout={({
+            nativeEvent: {
+              layout: { width, height },
+            },
+          }) => {
+            viewLayout.value = [width - 40, height - 40];
+          }}
+        >
+          <ScrollView contentInsetAdjustmentBehavior="automatic">
+            <Animated.View style={animatedStyles}>
+              <TouchableOpacity
+                style={styles.box}
+                onPress={() => {
+                  offset.value = [Math.random(), Math.random()];
+                }}
+              />
+            </Animated.View>
+          </ScrollView>
+        </View>
       </SafeAreaView>
-    </>
+    </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  safeArea: {
+    flex: 1,
+  },
+  content: {
+    flex: 1,
+  },
+  box: {
+    backgroundColor: "#056674",
+    borderRadius: 10,
+    width: 40,
+    height: 40,
+  },
+});
 
 export default App;
